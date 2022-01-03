6da422057fda3aa19c74a6b63def2d3c78bdf399
13wiki07 <wikzel13@gmail.com>
2022-01-03 10:31:53 +0100

    dodajemy plik w nowym folderze i tworzymy pliki

---
+++ DataGrid/Dane/WindowZPliku.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace DataGrid.Dane
+{
+    /// <summary>
+    /// Logika interakcji dla klasy WindowZPliku.xaml
+    /// </summary>
+    public partial class WindowZPliku : Window
+    {
+        // plik z danymi
+        private string plik1 = @"..\..\dane\Produkty.xml";
+        // plik na wyniki
+        private string plik2 = @"..\..\dane\Produkty2.xml";
+        private XElement wykazProduktow;
+        
+        public WindowZPliku()
+        {
+            InitializeComponent();
+        }
+    }
+}
