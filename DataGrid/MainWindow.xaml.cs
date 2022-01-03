3379158bf19fc8dd4d1fe27e66ea4df6e654fd8b
13wiki07 <wikzel13@gmail.com>
2022-01-03 08:23:13 +0100

    Dodaj pliki projektów.

---
+++ DataGrid/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Produkt> ListaProduktow = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            PrzygotujWiazanie();
+        }
+        private void PrzygotujWiazanie()
+        {
+            ListaProduktow = new ObservableCollection<Produkt>();
+            ListaProduktow.Add(new Produkt("Pan Tadeusz", true, 20, "klasyka"));
+            ListaProduktow.Add(new Produkt("Lalka", true, 25, "klasyka"));
+            ListaProduktow.Add(new Produkt("Kamienie na szaniec", false, 15, "sensacyjna"));
+            ListaProduktow.Add(new Produkt("Diuna", true, 24, "fantastyka"));
+            ListaProduktow.Add(new Produkt("Hobbit", false, 15, "fantastyka"));
+            gridProdukty.ItemsSource = ListaProduktow;
+        }
+    }
+}
---
+++ DataGrid/Produkt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGrid
+{
+    internal class Produkt
+    {
+        public String Nazwa {  get; set; }
+        public bool Dostepnosc {  get; set; }
+        public double Cena {  get; set; }
+        public String Kategoria {  get; set; }
+
+        public Produkt(string nazwa, bool dostepnosc, double cena, string kategoria)
+        {
+            this.Nazwa = nazwa;
+            this.Dostepnosc = dostepnosc;
+            this.Cena = cena;
+            this.Kategoria = kategoria;
+        }
+    
+    }
+}
